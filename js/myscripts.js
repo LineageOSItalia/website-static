9dbc715b44f258182c359c905a0b18ee6495626b
Joey <joey@lineageos.it>
2016-12-24 20:59:56 +0100

    [WIP] LAOS rebrand
    Signed-off-by: Joey Rizzoli <joey@lineageos.it>

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,19 +1,18 @@
-    $(document).ready(function(){
-      $('.parallax').parallax();
+$(document).ready(function() {
+    $('.parallax').parallax();
 
-      $('.button-collapse').sideNav({
-        menuWidth: 300, // Default is 240
+    $('.button-collapse').sideNav({
+        menuWidth: 300,
         edge: 'left', // Choose the horizontal origin
-        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      });
+    });
 
-      $('.slider').slider({
-          full_width: true,
-          height: 500,
-          indicators: false
-          });
+    $('.slider').slider({
+        full_width: true,
+        height: 500,
+        indicators: true
+    });
 
-      $('#modal1').leanModal({
+    $('#modal1').leanModal({
         dismissible: true, // Modal can be dismissed by clicking outside of the modal
         opacity: .5, // Opacity of modal background
         in_duration: 300, // Transition in duration
@@ -22,19 +21,20 @@
         complete: function() { alert('Closed'); } // Callback for Modal close
     });
 
+    $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrain_width: false, // Does not change width of dropdown to that of the activator
+        hover: false, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: false // Displays dropdown below the button
+    });
 
-      $('.dropdown-button').dropdown({
-          inDuration: 300,
-          outDuration: 225,
-          constrain_width: false, // Does not change width of dropdown to that of the activator
-          hover: false, // Activate on hover
-          gutter: 0, // Spacing from edge
-          belowOrigin: false // Displays dropdown below the button
-      });
+    $('.carousel').carousel();
 
+});
 
-      $(document).ready(function(){
-        $('.carousel').carousel();
-      });
-
+// Tell the user the page is WIP
+$(window).load(function() {
+    Materialize.toast('Pagina in costruzione, prova a tornare nei prossimi giorni', 120000);
 });
